36e513062a32a244c9be1f1f1fc97b13fc2c850d
Waard <Waard@localhost>
2012-06-20 22:57:14 +0200

    'working' corridor walk in xabsl and java

--- JavaXabslImplementation/src/jxi/world/World.java
+++ JavaXabslImplementation/src/jxi/world/World.java
@@ -18,22 +18,32 @@ public class World
     private double robotLaserMinNW;
     /** The minimum value for the NNW region of the lasersensor */
     private double robotLaserMinNNW;
+    /** The minimum value for the N region of the lasersensor */
+    private double robotLaserMinN;
     /** The minimum value for the NNE region of the lasersensor */
     private double robotLaserMinNNE;
     /** The minimum value for the NE region of the lasersensor */
     private double robotLaserMinNE;
     /** The minimum value for the ENE region of the lasersensor */
     private double robotLaserMinENE;
+    /** The lowest value of all laser beams */
+    private double robotLaserMin;
+    /** The highest value of all laser beams */
+    private double robotLaserMax;
+
     /** Time spent waiting with basic behavior Wait */
     private double stayed;
     /** The angle the world had the position before now */
     private double lastAngle;
     /** Ammount turned for basic behavior Drive Circle */
     private double ammount_turned;
-    /** The robot's possible behaviors */
+    /** The robot's possible behaviors. When a new behavior is added to the
+     * xabsl-hierarchy, it should be added in this enum as wel as the xabsl-enum
+     */
     public enum Behaviors 
     {
         DRIVE_CIRCLE,
+        WALK_CORRIDOR,
         UNEXISTANT_BEHAVIOR
     }
     /** Enumerated version of the robots current behavior */
@@ -73,10 +83,15 @@ public class World
     {
         this.lastAngle = angle;
     }
+    /** The setter for the current behavior. this one should always be editted
+     * before adding a behavior to the xabsl-hierarchy 
+     */
     public void setCurrent_behavior(String behavior)
     {
         if(behavior.equals("drive_circle"))
             current_behavior = Behaviors.DRIVE_CIRCLE;
+        else if(behavior.equals("walk_corridor"))
+            current_behavior = Behaviors.WALK_CORRIDOR;
     }
     public void setLaserMinWNW(double value)
     {
@@ -90,6 +105,10 @@ public class World
     {
         this.robotLaserMinNNW = value;
     }
+    public void setLaserMinN(double value)
+    {
+        this.robotLaserMinN = value;
+    }
     public void setLaserMinNNE(double value)
     {
         this.robotLaserMinNNE = value;
@@ -102,6 +121,15 @@ public class World
     {
         this.robotLaserMinENE = value;
     }
+    public void setLaserMin(double value)
+    {
+        this.robotLaserMin = value;
+    }
+    public void setLaserMax(double value)
+    {
+        this.robotLaserMax = value;
+    }
+   
 
 // }}}
 
@@ -146,6 +174,10 @@ public class World
     {
         return this.robotLaserMinNNW;
     }
+    public double getLaserMinN()
+    {
+        return this.robotLaserMinN;
+    }
     public double getLaserMinNNE()
     {
         return this.robotLaserMinNNE;
@@ -157,6 +189,14 @@ public class World
     public double getLaserMinENE()
     {
         return this.robotLaserMinENE;
+    }
+    public double getLaserMin()
+    {
+        return this.robotLaserMin;
+    }
+    public double getLaserMax()
+    {
+        return this.robotLaserMax;
     }
 
 // }}}
